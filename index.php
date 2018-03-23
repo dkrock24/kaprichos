6bf0c73a00aa1152d6c3116b6a8be29e7d8e3c1c
dkrock24 <rgutierreztejada@gmail.com>
2018-03-22 22:31:56 -0600

    Index - Slider / Destacaods

--- index.php
+++ index.php
@@ -1000,26 +1000,28 @@ $(document).ready(function() {
   </div>
   <!-- //header --> 
   <!-- banner -->
-  <div class="banner">
-    <div id="kb" class="carousel kb_elastic animate_text kb_wrapper" data-ride="carousel" data-interval="6000" data-pause="hover">
+  <div class="container">
+      <div class="row">
+          <div class="banner">
+    <div id="kb" class="carousel kb_elastic animate_text kb_wrapper" data-ride="carousel"   data-interval="6000" data-pause="hover">
       <!-- Wrapper-for-Slides -->
             <div class="carousel-inner" role="listbox">  
                 <div class="item active"><!-- First-Slide -->
-                    <img src="images/5.jpg" alt="" class="img-responsive" />
+                    <img src="assets/images/5.jpg" alt="" class="img-responsive" />
                     <div class="carousel-caption kb_caption kb_caption_right">
                         <h3 data-animation="animated flipInX">Flat <span>50%</span> Discount</h3>
                         <h4 data-animation="animated flipInX">Hot Offer Today Only</h4>
                     </div>
                 </div>  
                 <div class="item"> <!-- Second-Slide -->
-                    <img src="images/8.jpg" alt="" class="img-responsive" />
+                    <img src="assets/images/8.jpg" alt="" class="img-responsive" />
                     <div class="carousel-caption kb_caption kb_caption_right">
                         <h3 data-animation="animated fadeInDown">Our Latest Fashion Editorials</h3>
                         <h4 data-animation="animated fadeInUp">cupidatat non proident</h4>
                     </div>
                 </div> 
                 <div class="item"><!-- Third-Slide -->
-                    <img src="images/3.jpg" alt="" class="img-responsive"/>
+                    <img src="assets/images/3.jpg" alt="" class="img-responsive"/>
                     <div class="carousel-caption kb_caption kb_caption_center">
                         <h3 data-animation="animated fadeInLeft">End Of Season Sale</h3>
                         <h4 data-animation="animated flipInX">cupidatat non proident</h4>
@@ -1028,7 +1030,7 @@ $(document).ready(function() {
             </div> 
             <!-- Left-Button -->
             <a class="left carousel-control kb_control_left" href="#kb" role="button" data-slide="prev">
-        <span class="fa fa-angle-left kb_icons" aria-hidden="true"></span>
+            <span class="fa fa-angle-left kb_icons" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a> 
             <!-- Right-Button -->
@@ -1036,46 +1038,98 @@ $(document).ready(function() {
                 <span class="fa fa-angle-right kb_icons" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
             </a> 
-        </div>
-    <script src="js/custom.js"></script>
-  </div>
+    </div>
+      <script src="assets/js/custom.js"></script>
+      </div>
+      </div>
+  </div>  
   <!-- //banner -->  
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+
+        <div class="row">
+          <div class="col-md-12 focus-grid"> 
+            <a href="products.html" class="wthree-btn"> 
+              <div class="focus-image"><i class="fa fa-facebook"></i></div>
+              <h4 class="clrchg">Facebook</h4> 
+            </a>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12 focus-grid"> 
+            <a href="products.html" class="wthree-btn"> 
+              <div class="focus-image"><i class="fa fa-instagram"></i></div>
+              <h4 class="clrchg">Instragram</h4> 
+            </a>
+          </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-12 focus-grid"> 
+              <a href="products.html" class="wthree-btn"> 
+                <div class="focus-image"><i class="fa fa-pinterest"></i></div>
+                <h4 class="clrchg">Pinteres</h4> 
+              </a>
+            </div>
+        </div>
+      </div>
+      <div class="col-md-4 focus-grid">
+        
+          <a href="products.html" class="wthree-btn"> 
+            <div class="focus-image"><i class="fa fa-mobile"></i></div>
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+
+          </a>
+     
+      </div>
+      <div class="col-md-4 focus-grid">
+        
+          <a href="products.html" class="wthree-btn"> 
+            <div class="focus-image"><i class="fa fa-mobile"></i></div>
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            <h4 class="clrchg">Mobiles</h4> 
+            
+          </a>
+     
+      </div>
+    </div>
+  </div>
+
+
   <!-- welcome -->
   <div class="welcome"> 
     <div class="container"> 
       <div class="welcome-info">
         <div class="bs-example bs-example-tabs" role="tabpanel" data-example-id="togglable-tabs">
-          <ul id="myTab" class=" nav-tabs" role="tablist">
-            <li role="presentation" class="active"><a href="#home" id="home-tab" role="tab" data-toggle="tab" >
-              <i class="fa fa-laptop" aria-hidden="true"></i> 
-              <h5>Electronics</h5>
-            </a></li>
-            <li role="presentation"><a href="#carl" role="tab" id="carl-tab" data-toggle="tab"> 
-              <i class="fa fa-female" aria-hidden="true"></i>
-              <h5>Fashion</h5>
-            </a></li>
-            <li role="presentation"><a href="#james" role="tab" id="james-tab" data-toggle="tab"> 
-              <i class="fa fa-gift" aria-hidden="true"></i>
-              <h5>Photo & Gifts</h5>
-            </a></li>
-            <li role="presentation"><a href="#decor" role="tab" id="decor-tab" data-toggle="tab"> 
-              <i class="fa fa-home" aria-hidden="true"></i>
-              <h5>Home Decor</h5>
-            </a></li>
-            <li role="presentation"><a href="#sports" role="tab" id="sports-tab" data-toggle="tab"> 
-              <i class="fa fa-motorcycle" aria-hidden="true"></i>
-              <h5>Sports</h5>
-            </a></li> 
-          </ul>
+
           <div class="clearfix"> </div>
-          <h3 class="w3ls-title">Featured Products</h3>
+          <h3 class="w3ls-title">Destacados</h3>
           <div id="myTabContent" class="tab-content">
             <div role="tabpanel" class="tab-pane fade in active" id="home" aria-labelledby="home-tab">
               <div class="tabcontent-grids">  
                 <div id="owl-demo" class="owl-carousel"> 
                   <div class="item">
                     <div class="glry-w3agile-grids agileits"> 
-                      <a href="products.html"><img src="images/e1.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e1.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Audio speaker</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p>
@@ -1093,7 +1147,7 @@ $(document).ready(function() {
                   <div class="item">
                     <div class="glry-w3agile-grids agileits">
                       <div class="new-tag"><h6>Sale</h6></div>
-                      <a href="products.html"><img src="images/e2.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e2.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Refrigerator</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p> 
@@ -1111,7 +1165,7 @@ $(document).ready(function() {
                   <div class="item">
                     <div class="glry-w3agile-grids agileits"> 
                       <div class="new-tag"><h6>New</h6></div>
-                      <a href="products.html"><img src="images/e3.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e3.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Smart Phone</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p>
@@ -1128,7 +1182,7 @@ $(document).ready(function() {
                   </div>
                   <div class="item">
                     <div class="glry-w3agile-grids agileits"> 
-                      <a href="products.html"><img src="images/e4.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e4.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Digital Camera</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p>
@@ -1145,7 +1199,7 @@ $(document).ready(function() {
                   </div>
                   <div class="item">
                     <div class="glry-w3agile-grids agileits"> 
-                      <a href="products.html"><img src="images/e1.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e1.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Audio speaker</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p>
@@ -1163,7 +1217,7 @@ $(document).ready(function() {
                   <div class="item">
                     <div class="glry-w3agile-grids agileits">
                       <div class="new-tag"><h6>Sale</h6></div>
-                      <a href="products.html"><img src="images/e2.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e2.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Refrigerator</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p> 
@@ -1181,7 +1235,7 @@ $(document).ready(function() {
                   <div class="item">
                     <div class="glry-w3agile-grids agileits"> 
                       <div class="new-tag"><h6>New</h6></div>
-                      <a href="products.html"><img src="images/e3.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e3.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Smart Phone</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p>
@@ -1198,7 +1252,7 @@ $(document).ready(function() {
                   </div>
                   <div class="item">
                     <div class="glry-w3agile-grids agileits"> 
-                      <a href="products.html"><img src="images/e4.png" alt="img"></a>
+                      <a href="products.html"><img src="assets/images/e4.png" alt="img"></a>
                       <div class="view-caption agileits-w3layouts">           
                         <h4><a href="products.html">Digital Camera</a></h4>
                         <p>Lorem ipsum dolor sit amet consectetur</p>
