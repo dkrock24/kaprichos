64fd5696be799dc4c5e3c133f68a9d51f6d30e8d
Rafael.Gutierrez <rgutierreztejada@gmail.com>
2018-04-04 11:42:18 -0600

    New Text - Translate

--- lan/en.php
+++ lan/en.php
@@ -1,6 +1,10 @@
 <?php
 
 /* INDEX */
+//Title
+$messages['title'] 	= 'Log in- Kaprichos Flower shop';
+$messages['facebook'] 	= 'Follow us on Facebook for gifting advise and get special discounts for all our followers';
+$messages['entregas'] 	= 'Nationwide delivery';
 // Menu Top
 $messages['login'] 	= 'Login';
 $messages['Ingresar'] 	= 'Login';
@@ -41,17 +45,17 @@ $messages['aboutus'] = 'About Us';
 $messages['Direccion'] = 'Direcction';
 $messages['Products'] = 'Products';
 $messages['copy'] 	= 'All rights reserved';
-$messages['nosotrosDescripcion'] = 'Kapricho´s Floristería nació con el objetivo de  brindar arreglos florales innovadores, especializándonos en actualizar constantemente todos aquellos detalles que hacen su arreglo diferente. En Kapricho´s Floristería nos esmeramos porque sus sentimientos sean expresados con especial cuidado y garantizamos cubrir las expectativas.
-Todos nuestros arreglos son elaborados con materia prima de alta calidad, entre las clases de arreglos que le ofrecemos, tenemos: Frutales, primaverales, tropicales, orquídeas, globos sorpresa, chocolates, etc. Para toda ocasión, cumpleaños, felicitaciones, amistad, recuperación.
-También, nos ponemos a su disposición para cualquier tipo de evento, bodas, primera comunión, mesas de Honor, Baby shower decoraciones de salones, eventos de gobierno, Decoración de iglesias, etc. En donde le ofrecemos calidad, bajos costos y un alto sentido de responsabilidad.
-Ofrecemos nuestro servicio los siete días a la semana, por lo que, nos encontramos a su disposición en aquellos momentos inesperados y donde usted desea expresarse a través de un lindo detalle.';
+$messages['nosotrosDescripcion'] = 'Kapricho’s flower shop was established with the purpose of offering innovative flower arrangements, we are constantly updating every single detail to personalize your flower arrangement and make it look perfect. We give our best to express our clients feelings and put special care to guarantee the quality of our products. All of our products are elaborate with high quality raw materials, this includes, fruit baskets, spring flowers, tropicals, orchids, surprise balloons,chocolates, etc. At any special occasion, Birthdays, Congratulations, Sympathy, and  Get Well.
+We also offer a complete selection of personal for any event, weddings, first communion, honor table, baby shower, salon decoration, government events, church decoration etc. Where we give quality, low prices and high sense of responsibility. We are open seven days a week so that we are at your service at any unexpected moment that you want to express your feelings through an amazing detail
+.';
 
 
 
 $messages['telefonos'] = 'Phones';
 $messages['information'] = 'Information';
 $messages['politicas'] = 'POLITICAS';
-$messages['politicas2'] = 'Políticas de Reembolso';
+$messages['politicas2'] = 'Money back guarantee Policy';
+$messages['schedule'] = 'Open 24/7, Seven days a week';
 
 //Slider
 $messages['next'] = 'Next';
@@ -60,9 +64,9 @@ $messages['preview'] = 'preview';
 //menu left
 $messages['menuL'] = 'Search';
 $messages['catalogL'] = 'Catalog';
-$messages['ofertL'] = 'Oferts';
-$messages['questionL'] = 'Questions';
-$messages['whopayL'] = 'How Pay?';
+$messages['ofertL'] = 'Sale';
+$messages['questionL'] = 'Questions?';
+$messages['whopayL'] = 'Payment method';
 $messages['servicesL'] = 'Services';
 $messages['tipsL'] = 'Tips';
 $messages['contactusL'] = 'Contact Us';
@@ -91,6 +95,22 @@ $messages['sendC'] = 'Send';
 //Categorias
 $messages['cVer'] = 'Show';
 $messages['cAdd'] = 'Add';
+
+//Aviso
+
+$messages['aviso'] = 'If ever  product does not fit your needs, please do not doubt to return it. If the return is a result of a mistake from WWW. KAPRICHOSFLORISTERIA.COM, the full refund will be available 5 days after the date you submit the order. You should request a Product Return Authorization (PRA) number. You must send an email during those 5 days to apply for a refund. If the mistake is from WWW.KAPRICHOSFLORISTERIA.COM. Please make sure to indicate the nature of the mistake when requesting the PRA number and a customer service agent will provide all the detailed instructions on how to proceed.
+
+
+You should send the return form using a UPS, FedEx, TNT or DHL carrier. You will have to make sure that every item arrives in its original condition to the customer service center. There’s no refund after 5 business days. Any refund must be rejected if the product is returned without authorization. 
+
+Make sure to include the following attached to the return product: PRA number, name, address, email address, copy of the original invoice and any related information of the product that you had forgotten to include in the original PRA request.
+Returns won’t be available if the product arrives damaged or altered to the Customer Service Center, the prohas to be in the same condition in which it was delivered. 
+
+Once the commodity has been received by the Customer Service Center, your credit card will be credited back by the applicable amount. If you want to order another product instead of  the returned product, you will have to start a new order online.
+Please send your package to:
+Customer service Address and email address.
+Once the original product is received by the customer service center, your credit card will be credited back by the amount you paid, including the shipping costs. Please, wait from two to three weeks to get your credit card refunded. 
+';
 
 /* END INDEX */
 ?>  
